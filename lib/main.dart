75a6b283bd523e6c5d57d294a34778271d8b16d3
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-09 14:01:33 +0530

    color

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(Color.fromARGB(255, 188, 165, 248),
-            Color.fromARGB(255, 55, 255, 255)),
+        body: GradientContainer(Color.fromARGB(255, 30, 199, 250),
+            Color.fromARGB(255, 129, 23, 228)),
       ),
     ),
   );
